027c54b88a6321908f60acd33e0c4cd88bf6af3f
McKenna Brown <153936426+mckennabrown0224@users.noreply.github.com>
2025-02-26 14:27:02 -0700

    Here is the updated controller

--- Mission08_Team0304/Controllers/HomeController.cs
+++ Mission08_Team0304/Controllers/HomeController.cs
@@ -6,26 +6,86 @@ namespace Mission08_Team0304.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
+    private TasksContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    //constructor
+    public HomeController(TasksContext taskTemp) 
     {
-        _logger = logger;
+        _context = taskTemp;
     }
+    
+    //viewing the quadrant page
+    [HttpGet]
+    public IActionResult ViewQuadrants()
+    {
+        var temp = _context.Tasks.ToList(); //pulls List of tasks
+        return View(temp);
+    }
+    
+    //adding a task
+    [HttpGet]
+    public IActionResult AddATask()
+    {
+        return View("AddATask", new Task());
+    }
+    
+    //Saves valid new task instances to the database (if invalid, gives errors for correction)
+    [HttpPost]
+    public IActionResult AddATask(Task response)
+    {
+        if (ModelState.IsValid)
+        {
+            _context.Tasks.Add(response); 
+            _context.SaveChanges(); 
+            
+            return View("Confirmation", response);
+        }
+        else
+        {
+            return View("AddATask", response);
+        }
+    }
+    
+    //pulls up page to edit based on a task chosen by the user
+    [HttpGet]
+    public IActionResult Edit(int id)
+    {
+        var recordToEdit = _context.Tasks
+            .Single(x => x.TaskId == id);
+        
+        return View("AddATask", recordToEdit);
+    }
+    
+    //submits edited information back into the database
+    [HttpPost]
+    public IActionResult Edit(Task updatedInfo)
+    {
+        _context.Update(updatedInfo);
+        _context.SaveChanges();
+        
+        return RedirectToAction("ViewQuadrants");
+    }
+    
+    //pulls up confirmation page to delete the record
+    [HttpGet]
+    public IActionResult Delete(int id)
+    {
+        var recordToDelete = _context.Tasks
+            .Single(x => x.TaskId == id);
 
-    public IActionResult Index()
-    {
-        return View();
+        return View("Delete", recordToDelete);
     }
+    
+    //deletes the entry
+    [HttpPost]
+    public IActionResult Delete(Task deletingInfo)
+    {
+        _context.Tasks.Remove(deletingInfo);
+        _context.SaveChanges();
 
-    public IActionResult Privacy()
-    {
-        return View();
+        return RedirectToAction("ViewQuadrants");
     }
+}
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+
 }
