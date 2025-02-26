61739fb2144610a5c3d50180069b7be2ab9031e4
tklipp8 <turnerklippel@gmail.com>
2025-02-26 14:56:35 -0700

    Added various models and context.

---
+++ Mission08_Team0304/Models/Task.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mission08_Team0304.Models
+{
+    public class Task
+    {
+        [Key]
+        public int TaskId { get; set; }
+        [Required]
+        public string TaskName { get; set; }
+        public string? DueDate { get; set; }
+        [Required]
+        public int Quadrant { get; set; }
+        public string? Category { get; set; }
+        public bool? Completed { get; set; }
+    }
+}
--- Mission08_Team0304/Program.cs
+++ Mission08_Team0304/Program.cs
@@ -1,7 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Mission08_Team0304.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<TasksContext>(options =>
+{
+    options.UseSqlite(builder.Configuration["ConnectionStrings:TaskConnection"]);
+});
 
 var app = builder.Build();
 
