4d744dd5e6f0f8c580feee02147e83337909d5fe
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-02-28 13:04:21 -0700

    Added comments and made minor adjustment to AddTask in EFTaskRepository

--- Mission08_Team0304/Models/ITaskRepository.cs
+++ Mission08_Team0304/Models/ITaskRepository.cs
@@ -2,11 +2,13 @@ using Mission08_Team0304.Models;
 
 public interface ITaskRepository
 {
+    // Builds the interface to deal with adding, updating, deleting tasks
+    // and makes it so tasks are attached to their category
     List<Mission08_Team0304.Models.Task> Tasks { get; }
     IQueryable<Mission08_Team0304.Models.Task> GetTasksWithCategory();
-    void AddManager(Mission08_Team0304.Models.Task task);
+    void AddTask(Mission08_Team0304.Models.Task task);
     Mission08_Team0304.Models.Task GetTaskById(int id);
     void UpdateTask(Mission08_Team0304.Models.Task task);
     void DeleteTask(Mission08_Team0304.Models.Task task);
-    List<Category> GetCategories(); // Fetch categories
+    List<Category> GetCategories(); // Gets categories
 }
